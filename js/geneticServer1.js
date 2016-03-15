33e7faf51613f19035cb548e595178a9d6fdeb7e
Alexander Elyasov <aelyasov@ebay.com>
2016-03-15 14:39:39 +0100

    Decreased logging level from info to debug

--- js/geneticServer1.js
+++ js/geneticServer1.js
@@ -16,7 +16,7 @@ winston.level = 'info';
 
 http.createServer(function(request, response){
     
-    winston.log("info", "I got kicked");
+    winston.info("I got kicked");
     
     var data_ = '{ "jsFun":"function safeAdd(frameid) {var iframe = document.createElement(\\"iframe\\");var anchor = document.getElementById(\\"node\\");var frame = document.getElementById(frameid);iframe.setAttribute(\\"id\\",frameid);if (frame) {instrument._trace_.push(6);instrument._branchDistance_.push([6,Number(instrument._K_)]);instrument._trace_.push(7);frame.parentNode.removeChild(frame);} else {instrument._trace_.push(6);instrument._branchDistance_.push([6,_Number(K_)]);instrument._trace_.push(9);iframe.appendChild(anchor);}}", "jsMutFun":"var _K_ = 0;var _branchDistance_ = [];var _trace_ = [];function safeAdd(frameid,document) {var iframe = document.createElement(\\"iframe\\");var anchor = document.getElementById(\\"node\\");var frame = document.getElementById(frameid);iframe.setAttribute(\\"id\\",frameid);if (frame) {_branchDistance_.push([6,!_K_]);_trace_.push(7);frame.parentNode.removeChild(frame);} else {_branchDistance_.push([6,_K_]);_trace_.push(9);iframe.appendChild(anchor);}}"}';   
 
@@ -24,15 +24,15 @@ http.createServer(function(request, response){
     
     // var my_url = url.parse(request.url);
     var queryObject = url.parse(request.url,true).query;
-    winston.log("info", "queryObject:", queryObject);
+    winston.debug("queryObject:", queryObject);
 
     if (queryObject.init == "true") {
 	request.on('data', function(data) {
-	    winston.log("info", "Received the initial function:", data.toString());
+	    winston.debug("Received the initial function:", data.toString());
 	    
 	    // the function has to be encompassed by parencess to be evaluated by JavaScript
 	    // jsFun     = jsFun ? eval("(" + JSON.parse(data).jsFun + ")") : jsFun,
-	    winston.log("info", "jsFun before eval:\n", JSON.parse(data).jsFun);
+	    winston.debug("jsFun before eval:\n", JSON.parse(data).jsFun);
 	    var library = "var instrument = require(\"./instrumentLib.js\");\n"
 	    jsFun = library + JSON.parse(data).jsFun;
 	    
@@ -45,8 +45,8 @@ http.createServer(function(request, response){
 
     if (queryObject.mutation == "true") {
 	request.on('data', function(data) {
-	    winston.log("info", "Received the mutated function:\n", data.toString());
-	    winston.log("info", "# New mutation iteration: ", JSON.parse(data).mutN);
+	    winston.debug("Received the mutated function:\n", data.toString());
+	    winston.debug("# New mutation iteration: ", JSON.parse(data).mutN);
 	});
 	response.writeHeader(200, {"Content-Type": "text/plain"});
 	response.write("mutation response");
@@ -56,12 +56,12 @@ http.createServer(function(request, response){
 
     if (queryObject.genetic == "true") {
 	request.on('data', function(data) {
-	    winston.log("info", "Received arguments which are new population:", data.toString());
+	    winston.debug("Received arguments which are new population:", data.toString());
 
 	    // the function has to be encompassed by parencess to be evaluated by JavaScript
 	    // jsFun     = jsFun ? eval("(" + JSON.parse(data).jsFun + ")") : jsFun,
 	    jsFunArgs = JSON.parse(data).jsFunArgs.split("<|>"),	    
-	    winston.log("info", "jsFunArgs: ", jsFunArgs);
+	    winston.debug("jsFunArgs: ", jsFunArgs);
 
 	    //var intercept = fs.readFileSync("/home/alex/PROJECTS/FITTEST/Software/UtrechtUniv/tools/JSDomTest/js/interceptTest.js", "utf-8");
 
@@ -75,28 +75,28 @@ http.createServer(function(request, response){
 		    
 		    var _getElementById = document.getElementById;
 		    document.getElementById = function() {
-			winston.log("info", "id", arguments[0]);
+			winston.debug("id", arguments[0]);
 			environment.ids = environment.ids.concat(arguments[0]);
 			return _getElementById.apply(this, arguments);
 		    };
 
 		    _getElementsByClassName = document.getElementsByClassName;
 		    document.getElementsByClassName = function() {
-			winston.log("info", "class", arguments[0]);
+			winston.debug("class", arguments[0]);
 			environment.classes = environment.classes.concat(arguments[0].split(" "));
 			return _getElementsByClassName.apply(this, arguments);
 		    };
 		    
 		    _getElementsByName =  document.getElementsByName;
 		    document.getElementsByName = function() {
-			winston.log("info", "name", arguments[0]);
+			winston.debug("name", arguments[0]);
 			environment.names = environment.names.concat(arguments[0]);
 			return _getElementsByName.apply(this, arguments);
 		    };
 		    
 		    _getElementsByTagName = document.getElementsByTagName;
 		    document.getElementsByTagName = function() {
-			winston.log("info", "tag", arguments[0]);
+			winston.debug("tag", arguments[0]);
 			environment.tags = environment.tags.concat(arguments[0]);
 			return _getElementsByTagName.apply(this, arguments);
 		    };
@@ -105,7 +105,7 @@ http.createServer(function(request, response){
 		    // the hook for getElementsByTagNameNS ignores information about namespace
 		    _getElementsByTagNameNS = document.getElementsByTagNameNS;
 		    document.getElementsByTagNameNS = function() {
-			winston.log("info", "tag", arguments[1]);
+			winston.debug("tag", arguments[1]);
 			environment.tags = environment.tags.concat(arguments[1]);
 			return _getElementsByTagNameNS.apply(this, arguments);
 		    };
@@ -114,7 +114,7 @@ http.createServer(function(request, response){
 		    var _branchDistance_ = [];
 		    var _trace_ = [1];
 
-		    winston.log("info", "jsFun:\n", jsFun);
+		    winston.info("jsFun:\n", jsFun);
 		    eval(jsFun);
 
 		    try {
@@ -124,9 +124,9 @@ http.createServer(function(request, response){
 			winston.log("info", "Test function is exceptionally terminated with the message: %s", e.stack)
 		    }
 		    
-		    winston.log("info", "_trace_", _trace_);
-		    winston.log("info", " _branchDistance_",  _branchDistance_);
-		    winston.log("info", "environment: ", environment);
+		    winston.info("_trace_", _trace_);
+		    winston.debug(" _branchDistance_",  _branchDistance_);
+		    winston.debug("environment: ", environment);
 		    response.writeHeader(200, {"Content-Type": "text/plain"});
 		    response.write(JSON.stringify({_trace_:_trace_,  _branchDistance_: _branchDistance_}));
 		    response.end();    
@@ -137,9 +137,9 @@ http.createServer(function(request, response){
 
     if (queryObject.execute == "true") {
 	request.on('data', function(data) {
-	    winston.log("info", "Received genetically approved arguments", data.toString()); 
+	    winston.debug("Received genetically approved arguments", data.toString()); 
 	    jsFunArgs = JSON.parse(data).jsArgs.split("<|>"),	    
-	    winston.log("info", "jsFunArgs: ", jsFunArgs);
+	    winston.debug("jsFunArgs: ", jsFunArgs);
 	});
 	response.writeHeader(200, {"Content-Type": "text/plain"});
 	response.write("execution response");
@@ -147,4 +147,4 @@ http.createServer(function(request, response){
     }
     
 }).listen(7777);
-winston.log("info", "Server Running on 7777");
+winston.info("Server Running on 7777");
