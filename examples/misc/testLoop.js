ee4ba405ea8ee258d90141561ac4756ad4ce8c38
Alexander Elyasov <aelyasov@ebay.com>
2016-04-20 13:19:28 +0200

    New test examples

---
+++ examples/misc/testLoop.js
@@ -0,0 +1,11 @@
+/*t dom */
+function test() {
+  for(var row=0;row<2;row++) {
+    if (true) {
+      var obj = document.getElementById('square_'+row+'_'+col);
+    } else {
+      var spans = obj.getElementsByTagName('SPAN');
+    }
+  }
+  gameFinished=true;
+}
