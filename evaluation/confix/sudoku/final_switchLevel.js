dfc1011ad639770c2c051b62c97872d045209d84
Alexander Elyasov <aelyasov@ebay.com>
2017-06-19 10:52:52 +0200

    Processing case studies

---
+++ evaluation/confix/sudoku/final_switchLevel.js
@@ -0,0 +1,13 @@
+/*t dom:bool */
+function test(gameFinished) {
+    var linkObj = document.getElementsByTagName("BODY")[0];
+    var confirmSwitch = gameFinished;
+    if (confirmSwitch) {
+        var parentObj = linkObj.parentNode.parentNode;
+        var links = parentObj.getElementsByTagName("A");
+        for (var no = 0; no < links.length; no++) {
+            links[no].style.fontWeight = 'normal';
+        }
+        linkObj.style.fontWeight = 'bold';
+    }
+}
