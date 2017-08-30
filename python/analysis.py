a8de512549e39c371862e90f03d90977b6f9d6be
Alexander Elyasov <aelyasov@ebay.com>
2017-08-30 19:39:54 +0200

    Working on data analysis

---
+++ python/analysis.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import pandas as pd
+
+a = pd.read_csv('RequiredField.csv', sep=';')
+# print a
+b = pd.read_csv('RequiredField1.csv', sep=';')
+# print b
+b = b.drop('brunch', axis=1)
+merged = pd.concat([a,b], axis=1)
+print merged.iloc[:,1:21]
+merged['mean'] = merged.iloc[:,1:1].mean(axis=1)
+merged.to_csv('output.csv')
+print merged
