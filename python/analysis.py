5ab79df090b6b7339c40498fbf64db02fe22d0b9
Alexander Elyasov <aelyasov@ebay.com>
2017-09-06 17:18:43 +0200

    Script gather data about a run in one excel sheet.

--- python/analysis.py
+++ python/analysis.py
@@ -3,14 +3,22 @@
 import pandas as pd
 
 a = pd.read_csv('RequiredField.csv', sep=';')
-# print a
+print a
 b = pd.read_csv('RequiredField1.csv', sep=';')
-# print b
+print b
 b = b.drop('brunch', axis=1)
+result = a['brunch'].to_frame()
+print result
 merged = pd.concat([a,b], axis=1)
-print b.iloc[14:15]
-print b.iloc[14:15].mean(axis=1)
-# print merged.iloc[:,1:21]
-merged['mean'] = merged.iloc[:,1:1].mean(axis=1)
-merged.to_csv('output.csv')
+meged_numeric = merged.apply(pd.to_numeric, errors='coerce')
+result['mean'] = meged_numeric.mean(axis=1)
+result['max'] = meged_numeric.max(axis=1)
+result['min'] = meged_numeric.min(axis=1)
+result['median'] = meged_numeric.median(axis=1)
+print result
+headers = ['brunch', 'mean', 'max', 'min', 'median']
+result.to_csv('output.csv', index=False, header=headers)
 
+writer = pd.ExcelWriter('result.xlsx', engine='xlsxwriter')
+result.to_excel(writer, sheet_name='RequiredField')
+writer.save()
