6dde0060170f6ef9345a4bbbb6679fe75525c6e6
Alexander Elyasov <aelyasov@ebay.com>
2017-11-15 09:16:13 +0100

    Provided motivation example. Working on an evaluation.

--- evaluation/confix/sudoku/tests_isGameFinished.js
+++ evaluation/confix/sudoku/tests_isGameFinished.js
@@ -4,7 +4,7 @@ let doc, window, document;
 
 let dom1 = "<!DOCTYPE HTML><html><head><title>Test</title></head><body><div id='sudoku'></div></body></html>";
 
-let dom2 = "<!DOCTYPE HTML><html><head><title>Test</title></head><body><div id='sudoku'></div></body></html>";
+let dom2 = "<!DOCTYPE HTML><html><head><title>Test</title></head><body><div id='sudoku'><div></div></div></body></html>";
 
 let dom3 = "<!DOCTYPE HTML><html><head><title>Test</title></head><body><div id='sudoku'><div class='sudokuSquare'><span></span><span></span></div></div></body></html>";
 
@@ -45,16 +45,16 @@ function runTest(fun, dom, args = []) {
     Reflect.apply(fun, this, args);
 }
 
-// all branches: (1,8), (1,2), (2,3), (2,7), (4,5), (4,6), (8,9), (8,10)
+// all branches: *(1,8), *(1,2), *(2,3), *(2,7), *(4,5), *(4,6), *(8,9), (8,10)
 
-// cover branch (1,8), (8,9); path = [1,8,9,10]
+// cover branch *(1,8), *(8,9); path = [1,8,9,10]
 runTest(isGameFinished, dom1);
 
-// cover branch (1,2); path = [1,2,7,8,9,10]
+// cover branch *(1,2), (2,7), (8,9); path = [1,2,7,8,9,10]
 runTest(isGameFinished, dom2);
 
-// cover branch (2,3), (4,6); path = [1,2,3,4,6,7,8,9,10]
+// cover branch *(2,3), *(4,6), (8,9); path = [1,2,3,4,6,7,8,9,10]
 runTest(isGameFinished, dom3);
 
-// cover branch (4,5); path = [1,2,3,4,5,8,10]
+// cover branch (1,2), (2,3), *(4,5), *(8, 10); path = [1,2,3,4,5,8,10]
 runTest(isGameFinished, dom4);
